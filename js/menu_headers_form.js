fa91a488d45b19fd27c1865d03fab5085bf99bcc
zigzackattack <zackbabtkis@gmail.com>
2013-02-14 12:14:31 -0800

    added stylesheets, fixed update/create db entry issue, where menu header creation would initially fail, added customizable font size for each menu header

--- js/menu_headers_form.js
+++ js/menu_headers_form.js
@@ -1,55 +1,23 @@
-Drupal.behaviors.menu_headers = {
-	attach: function(){
-		//checkElements();
-		addHeaders();
-	}
-};
-/*var Pair = function() {
-	var self = this;
-	this.init = function(title, checkbox) {
-		this.title = title;
-		this.checkbox = checkbox;
-		this.addOpenClose();
-		this.listen();
-	};
-	this.listen = function() {
-		this.checkbox.addEventListener('click', self.addOpenClose, false);
-	};
-	this.addOpenClose = function() {
-		if(self.checkbox.checked === true) {
-			self.title.style.display = 'block';
-		} else {
-			self.title.style.display = 'none';
-		}
-	};
-};	
-
-function checkElements() {
-	var pairObjects = [];
-	var pairs = document.getElementsByClassName('menu-pair');
-	for(i in pairs) {
-		var p = pairs[i].children;
-		if(pairs[i].children) {
-			var title = p[0].children[0];
-			var checkbox = p[1].children[0];
-			currPair = new Pair().init(title, checkbox);
-			pairObjects[i] = currPair;
-		}
-	}
-}
-*/
-
-function addHeaders(){
-	headers = Drupal.settings.menu_headers;
-	for(i in headers) {
-		var item = document.getElementsByClassName('menu-item-' + headers[i].mlid)[0];
-		if(item) {
-			var header = document.createElement('h3');
-			var headerText = document.createTextNode(headers[i].header);
-			header.appendChild(headerText);
-			header.style.margin = 0;
-			header.style.textAlign = 'center';
-			item.insertBefore(header, item.firstChild);
-		}
-	};
-}
+(function ($) {
+  Drupal.behaviors.menu_headers = {
+    attach: function(){
+      //checkElements();
+      addHeaders();
+    }
+  };
+  function addHeaders(){
+    headers = Drupal.settings.menu_headers;
+    for(i in headers) {
+      var item = $('.menu-item-' + headers[i].mlid)[0];
+      var text = headers[i].header;
+      var fontSize = 'h' + headers[i].size;
+      if(item && text) {
+        var header = document.createElement(fontSize);
+        var headerText = document.createTextNode(text);
+        header.appendChild(headerText);
+        header.className = 'menu-headers menu-headers-' + fontSize;
+        item.insertBefore(header, item.firstChild);
+      }
+    };
+  }
+})(jQuery);
